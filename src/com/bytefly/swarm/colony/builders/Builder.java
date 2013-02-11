968998bc63e6847bf1cabb4f70de1325c0e4c2d5
Bob Pascazio <bob@bytefly.com>
2013-02-11 17:09:30 -0500

    more xml parsing, testflight params

--- src/com/bytefly/swarm/colony/builders/Builder.java
+++ src/com/bytefly/swarm/colony/builders/Builder.java
@@ -4,6 +4,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 import com.bytefly.swarm.colony.Status;
 import com.bytefly.swarm.colony.models.Project;
 import com.bytefly.swarm.colony.util.Config;
@@ -14,31 +22,38 @@ public class Builder {
 
 	public Project p;
 
-	String toList;
-	String toFailList;
+	String toList = "null@null.com";
+	String toFailList= "null@null.com";
+
+	String tfGroup = "x";
+	String tfDist= "y";
 
 	public static final int BUILDER_TYPE_GENERIC = 0;
 	public static final int BUILDER_TYPE_XCODE = 1;
 	public static final int BUILDER_TYPE_ANDROID = 2;
-	
+
 	public void sendFailureEmail() {
 
 		try {
-			String name = p.BaseName+"-"+p.Version+"-";
+			String name = p.BaseName + "-" + p.Version + "-";
 			String owner = "bytefly";
 			String repo = p.BaseName;
 			String to = toFailList;
-			String cmd = 
-					String.format(Config.getStringValue(Config.SWARM_SEND_FAILURE_EMAIL), 
-							name, p.buildNum, this.p.BaseName+".apk", owner, repo, to);
-			Debug.Log(Debug.TRACE, "Executing "+cmd);
-			Process pr = Runtime.getRuntime().exec(cmd,null,new File(this.p.BaseName));
-			pr.waitFor(); 
+			String cmd = String
+					.format(Config
+							.getStringValue(Config.SWARM_SEND_FAILURE_EMAIL),
+							name, p.buildNum, this.p.BaseName + ".apk", owner,
+							repo, to);
+			Debug.Log(Debug.TRACE, "Executing " + cmd);
+			Process pr = Runtime.getRuntime().exec(cmd, null,
+					new File(this.p.BaseName));
+			pr.waitFor();
 		} catch (Exception e) {
-			Debug.Log(Debug.INFO, "Exception caught running repoGet "+e.toString());
-		}	
+			Debug.Log(Debug.INFO,
+					"Exception caught running repoGet " + e.toString());
+		}
 	}
-	
+
 	public void notifyEmail() {
 		try {
 			Debug.Log(
@@ -46,8 +61,9 @@ public class Builder {
 					"getting url "
 							+ Config.getStringValue(Config.SWARM_NOTIFY_EMAIL_CMD));
 			HttpConnector hc = new HttpConnector();
-			String r = hc.getURL(Config.getStringValue(Config.SWARM_NOTIFY_EMAIL_CMD));
-			Debug.Log(Debug.TRACE, "notifyEmail result="+r);
+			String r = hc.getURL(Config
+					.getStringValue(Config.SWARM_NOTIFY_EMAIL_CMD));
+			Debug.Log(Debug.TRACE, "notifyEmail result=" + r);
 		} catch (Exception e) {
 			Debug.Log(Debug.INFO,
 					"Exception caught running notifyEmail " + e.toString());
@@ -64,8 +80,8 @@ public class Builder {
 			Process pr = Runtime.getRuntime().exec(
 					Config.getStringValue(Config.SWARM_CLEAN_REPO_CMD) + " "
 							+ p.BaseName);
-			pr.waitFor(); 
-			Debug.Log(Debug.TRACE, "repoClean result="+getOutAndErrStream(pr));
+			pr.waitFor();
+			Debug.Log(Debug.TRACE, "repoClean result=" + getOutAndErrStream(pr));
 		} catch (Exception e) {
 			Debug.Log(Debug.INFO,
 					"Exception caught running repoClean " + e.toString());
@@ -80,10 +96,11 @@ public class Builder {
 					"Executing "
 							+ Config.getStringValue(Config.SWARM_GIT_UPDATE_CMD));
 			Process pr = Runtime.getRuntime().exec(
-					Config.getStringValue(Config.SWARM_GIT_UPDATE_CMD),null,new File(this.p.BaseName));
-			pr.waitFor(); 
+					Config.getStringValue(Config.SWARM_GIT_UPDATE_CMD), null,
+					new File(this.p.BaseName));
+			pr.waitFor();
 			String result = getOutAndErrStream(pr).replace("\n", "");
-			Debug.Log(Debug.TRACE, "repoUpdate result="+result);
+			Debug.Log(Debug.TRACE, "repoUpdate result=" + result);
 		} catch (Exception e) {
 			Debug.Log(Debug.INFO,
 					"Exception caught running repoUpdate " + e.toString());
@@ -101,52 +118,138 @@ public class Builder {
 			Process pr = Runtime.getRuntime().exec(
 					Config.getStringValue(Config.SWARM_GIT_CLONE_CMD) + " "
 							+ p.Repo);
-			pr.waitFor(); 
-			Debug.Log(Debug.TRACE, "repoClone result="+getOutAndErrStream(pr));
+			pr.waitFor();
+			Debug.Log(Debug.TRACE, "repoClone result=" + getOutAndErrStream(pr));
 		} catch (Exception e) {
 			Debug.Log(Debug.INFO,
 					"Exception caught running repoGet " + e.toString());
 		}
 	}
 
-	protected String getOutAndErrStream(Process p){
+	protected String getOutAndErrStream(Process p) {
 
 		StringBuffer cmd_out = new StringBuffer("");
-		if(p != null){
-			BufferedReader is = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		if (p != null) {
+			BufferedReader is = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
 			String buf = "";
-			try{
-				while((buf = is.readLine()) != null){
+			try {
+				while ((buf = is.readLine()) != null) {
 					cmd_out.append(buf);
-					cmd_out.append (System.getProperty("line.separator"));
+					cmd_out.append(System.getProperty("line.separator"));
 				}
 				is.close();
-				is = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-				while((buf = is.readLine()) != null){
+				is = new BufferedReader(new InputStreamReader(
+						p.getErrorStream()));
+				while ((buf = is.readLine()) != null) {
 					cmd_out.append(buf);
 					cmd_out.append("\n");
 				}
 				is.close();
-			}catch(Exception e){
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} 
+		}
 		return cmd_out.toString();
 	}
-	
+
+	public class ReadXMLFile {
+
+		public void execute(String fname) {
+
+			try {
+
+				SAXParserFactory factory = SAXParserFactory.newInstance();
+				SAXParser saxParser = factory.newSAXParser();
+
+				DefaultHandler handler = new DefaultHandler() {
+
+					boolean tos = false;
+					boolean tof = false;
+					boolean tfg = false; //group
+					boolean tfd = false; //distribution list
+
+					public void startElement(String uri, String localName,
+							String qName, org.xml.sax.Attributes attributes)
+							throws SAXException {
+
+						if (qName.equalsIgnoreCase("to_success")) {
+							tos = true;
+						}
+
+						if (qName.equalsIgnoreCase("to_failure")) {
+							tof = true;
+						}
+						if (qName.equalsIgnoreCase("testflight_token")) {
+							tfg = true;
+						}
+
+						if (qName.equalsIgnoreCase("testflight_distribution_group")) {
+							tfd = true;
+						}
+					}
+
+					public void endElement(String uri, String localName,
+							String qName) throws SAXException {
+
+					}
+
+					public void characters(char ch[], int start, int length)
+							throws SAXException {
+
+						if (tos) {
+							tos = false;
+							toList = new String(ch, start, length);
+						}
+
+						if (tof) {
+							tof = false;
+							toFailList = new String(ch, start, length);
+						}
+
+						if (tfg) {
+							tfg = false;
+							tfGroup = new String(ch, start, length);
+						}
+
+						if (tfd) {
+							tfd = false;
+							tfDist = new String(ch, start, length);
+						}
+
+					}
+
+				};
+
+				saxParser.parse(fname, handler);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
 	public void loadSwarmXML() {
-		
+
 		try {
-			Debug.Log(Debug.TRACE, "Executing "+Config.getStringValue(Config.SWARM_DUMP_XML));
-			Process pr = Runtime.getRuntime().exec(Config.getStringValue(Config.SWARM_DUMP_XML),null,new File(this.p.BaseName));
-			pr.waitFor(); 
-			toList = new String(getOutAndErrStream(pr));
-			toList = toList.replace("\n", "");
-			if (toList.equals("cat: swarm.xml: No such file or directory")) toList="";
-			Debug.Log(Debug.TRACE, "toList="+toList);
-			toFailList = toList;
+
+			ReadXMLFile r = new ReadXMLFile();
+			r.execute(this.p.BaseName + "/swarm.xml");
+			
+			toList=toList.replace("\n", "");
+			toFailList = toFailList.replace("\n", "");
+			tfGroup = tfGroup.replace("\n", "");
+			tfDist = tfDist.replace("\n", "");
+			
+			Debug.Log(Debug.TRACE, "toList " + toList);
+			Debug.Log(Debug.TRACE, "toFailList " + toFailList);
+			Debug.Log(Debug.TRACE, "tfGroup " + tfGroup);
+			Debug.Log(Debug.TRACE, "tfDist " + tfDist);
 		} catch (Exception e) {
-			Debug.Log(Debug.INFO, "Exception caught running repoGet "+e.toString());
+			Debug.Log(Debug.INFO,
+					"Exception caught running repoGet " + e.toString());
 		}
 	}
 }
