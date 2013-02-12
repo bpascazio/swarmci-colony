d0baf2f7479d106e0f5e379db74255864c634226
Bob Pascazio <bob@bytefly.com>
2013-02-12 09:42:29 -0500

    updated command line

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo cp swarm /usr/bin/swarm
+sudo chmod 755 /usr/bin/swarm 
+sudo cp build/jar/Swarm.jar /usr/bin/Swarm.jar
+sudo chmod 755 /usr/bin/Swarm.jar
--- src/com/bytefly/swarm/CommandLine.java
+++ src/com/bytefly/swarm/CommandLine.java
@@ -1,16 +1,37 @@
 package com.bytefly.swarm;
 
-import com.bytefly.swarm.colony.util.Debug;
-import com.bytefly.swarm.colony.util.Version;
+import java.util.Scanner;
+
+import com.bytefly.swarm.util.Debug;
 
 public class CommandLine {
 	public static void main(String[] args) {
 		if (args.length==0) {
 			usage();
+		} else if (args[0].equals("version")) {
+			version();
+		} else if (args[0].equals("create")) {
+			create();
 		}
 	}
 	public static void usage() {
-		Debug.Log(Debug.INFO, "swarm ");
+		Debug.Log(Debug.INFO, "Usage: swarm [options]");
+		Debug.Log(Debug.INFO, "\nOptions:\n");
+		Debug.Log(Debug.INFO, "create  # Creates a new swarm project configuration");		
+		Debug.Log(Debug.INFO, "start   # Sets Started project state so builds are sent out normally");
+		Debug.Log(Debug.INFO, "pause   # Pauses project so success/failures are only sent to swarm owner");
+		Debug.Log(Debug.INFO, "stop    # Stops project so no more builds are run");
+		Debug.Log(Debug.INFO, "status  # Displays swarm status and runs tate of current project");
+		Debug.Log(Debug.INFO, "log     # Displays activity log from swarm cloud server");
+		Debug.Log(Debug.INFO, "version # Displays version information");
+	}
+	public static void version() {
 		Debug.Log(Debug.INFO, "Version is "+Version.getVersion()+" Build "+Version.getBuildNum());		
 	}
+	public static void create() {
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
+        System.out.print("Password: ");
+        String password = scanner.nextLine();	}
 }
