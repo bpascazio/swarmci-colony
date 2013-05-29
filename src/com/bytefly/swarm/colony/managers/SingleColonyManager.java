182be57c11263c55b44e724563e0dad8a48e5804
Bob Pascazio <bob@bytefly.com>
2013-05-28 20:22:05 -0400

    fixup of NPE in single colony

--- src/com/bytefly/swarm/colony/managers/SingleColonyManager.java
+++ src/com/bytefly/swarm/colony/managers/SingleColonyManager.java
@@ -12,7 +12,8 @@ public class SingleColonyManager extends Manager {
 	Manager gm;
 	Manager bm;
 
-	public SingleColonyManager(Manager _cm, Manager _pm, Manager _gm, Manager _bm) {
+	public SingleColonyManager(Manager _cm, Manager _pm, Manager _gm,
+			Manager _bm) {
 		super(true);
 		cm = _cm;
 		pm = _pm;
@@ -32,10 +33,16 @@ public class SingleColonyManager extends Manager {
 		Work w = null;
 
 		while (running) {
-			Status.qsize_build_mgr = bm.getQSize();
-			Status.qsize_cloud_mgr = cm.getQSize();
-			Status.qsize_git_mgr = gm.getQSize();
-			Status.qsize_proj_mgr = pm.getQSize();
+			try {
+				Status.qsize_build_mgr = bm.getQSize();
+				Status.qsize_cloud_mgr = cm.getQSize();
+				Status.qsize_git_mgr = gm.getQSize();
+				Status.qsize_proj_mgr = pm.getQSize();
+			} catch (Exception e) {
+				Debug.Log(Debug.INFO,
+						"SingleColonyManager exception with managers.");
+				stop();
+			}
 			//
 			// Swarm main processing is a thread that waks up 4 times a second
 			// (for now).
@@ -52,9 +59,10 @@ public class SingleColonyManager extends Manager {
 			if (ccloud == 0) {
 
 				// send command to git manager
-				w = new Work(Work.WORK_ITEM_CLOUD_CHECK_CONNECTION);
 				try {
-					Debug.Log(Debug.TRACE, "SingleColonyManager handing to cloud manager.");					
+					w = new Work(Work.WORK_ITEM_CLOUD_CHECK_CONNECTION);
+					Debug.Log(Debug.TRACE,
+							"SingleColonyManager handing to cloud manager.");
 					cm.put(w);
 				} catch (Exception e) {
 					Debug.Log(Debug.INFO,
@@ -72,12 +80,13 @@ public class SingleColonyManager extends Manager {
 			if (cproject == 0) {
 
 				// Only issue project scan if cloud is connected
-				if (((CloudManager)cm).connected()) {
+				if (((CloudManager) cm).connected()) {
 
 					// send command to project manager
-					w = new Work(Work.WORK_ITEM_PROJECT_FETCH_PROJECTS);
 					try {
-						Debug.Log(Debug.TRACE, "SingleColonyManager handing to project manager.");
+						w = new Work(Work.WORK_ITEM_PROJECT_FETCH_PROJECTS);
+						Debug.Log(Debug.TRACE,
+								"SingleColonyManager handing to project manager.");
 						pm.put(w);
 					} catch (Exception e) {
 						Debug.Log(Debug.INFO,
@@ -88,7 +97,7 @@ public class SingleColonyManager extends Manager {
 				} else {
 					Debug.Log(Debug.INFO,
 							"SingleColonyManager not pulling projects, cloud is not connected.");
-					
+
 				}
 
 				// reset the counter
@@ -102,12 +111,13 @@ public class SingleColonyManager extends Manager {
 			if (cscan == 0) {
 
 				// Only issue git change scan if cloud is connected
-				if (((CloudManager)cm).connected()) {
+				if (((CloudManager) cm).connected()) {
 
 					// send command to git manager
-					w = new Work(Work.WORK_ITEM_GIT_SCAN_PROJECTS);
 					try {
-						Debug.Log(Debug.TRACE, "SingleColonyManager handing to git manager.");
+						w = new Work(Work.WORK_ITEM_GIT_SCAN_PROJECTS);
+						Debug.Log(Debug.TRACE,
+								"SingleColonyManager handing to git manager.");
 						gm.put(w);
 					} catch (Exception e) {
 						Debug.Log(Debug.INFO,
@@ -118,42 +128,45 @@ public class SingleColonyManager extends Manager {
 				} else {
 					Debug.Log(Debug.INFO,
 							"SingleColonyManager not scanning projects, cloud is not connected.");
-					
+
 				}
 
 				// reset the counter
 				cscan = Config.getIntValue(Config.SWARM_GIT_CHECK_FREQ)
 						+ Config.getIntValue(Config.SWARM_MGR_CHECK_FREQ);
-				
-				// if clife is not -1 (infinite run) then we decrement it till it gets to 0
-				if (clife>=0) {
+
+				// if clife is not -1 (infinite run) then we decrement it till
+				// it gets to 0
+				if (clife >= 0) {
 					clife--;
-					Debug.Log(Debug.TRACE, "SingleColonyManager lifetime is "+clife);
-					if(clife==0) {
-						running=false;
+					Debug.Log(Debug.TRACE, "SingleColonyManager lifetime is "
+							+ clife);
+					if (clife == 0) {
+						running = false;
 						System.exit(1);
 					}
 				} else {
-					Debug.Log(Debug.TRACE, "SingleColonyManager lifetime is forever");
+					Debug.Log(Debug.TRACE,
+							"SingleColonyManager lifetime is forever");
 				}
 			} else {
 				cscan = cscan - Config.getIntValue(Config.SWARM_MGR_CHECK_FREQ);
 			}
 
 			// Check to see if we've gone to a connected state, this auto clears
-			if (((CloudManager)cm).connectTriggerLatchTransition()) {
-				
+			if (((CloudManager) cm).connectTriggerLatchTransition()) {
+
 				// Reset everthing since now connected
 				cproject = cscan = 0;
-				
+
 				Debug.Log(Debug.INFO,
 						"SingleColonyManager resetting due to new connection state.");
 			}
-			
+
 			try {
 
 				Status.counter_heartbeat++;
-				
+
 				// Time to sleep for a bit.
 				Thread.sleep(Config.getIntValue(Config.SWARM_MGR_CHECK_FREQ));
 
@@ -163,5 +176,6 @@ public class SingleColonyManager extends Manager {
 
 		}
 		Debug.Log(Debug.INFO, "SingleColonyManager no longer running.");
+		System.exit(1);
 	}
 }
